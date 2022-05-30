646f1e2b33f678065857a5a3c4e9940490dfa486
smhasnat <smhasnat.01@gmail.com>
2022-05-30 18:32:53 +0600

    Hello Hasnat!

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-const hourEl = document.querySelector('.hour')
-const minuteEl = document.querySelector('.minute')
-const secondEl = document.querySelector('.second')
-const timeEl = document.querySelector('.time')
-const dateEl = document.querySelector('.date')
-const toggle = document.querySelector('.toggle')
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
 
 const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-]
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
-]
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-toggle.addEventListener('click', (e) => {
-  const html = document.querySelector('html')
-  if (html.classList.contains('dark')) {
-    html.classList.remove('dark')
-    e.target.innerHTML = 'Dark mode'
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
   } else {
-    html.classList.add('dark')
-    e.target.innerHTML = 'Light mode'
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
   }
-})
+});
 
 function setTime() {
-  const time = new Date()
-  const month = time.getMonth()
-  const day = time.getDay()
-  const date = time.getDate()
-  const hours = time.getHours()
-  const hoursForClock = hours % 12
-  const minutes = time.getMinutes()
-  const seconds = time.getSeconds()
-  const ampm = hours >= 12 ? 'PM' : 'AM'
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
 
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     hoursForClock,
@@ -57,33 +57,33 @@ function setTime() {
     11,
     0,
     360
-  )}deg)`
+  )}deg)`;
   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     minutes,
     0,
     59,
     0,
     360
-  )}deg)`
+  )}deg)`;
   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     seconds,
     0,
     59,
     0,
     360
-  )}deg)`
+  )}deg)`;
 
   timeEl.innerHTML = `${hoursForClock}:${
     minutes < 10 ? `0${minutes}` : minutes
-  } ${ampm}`
-  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+  } ${ampm}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
 }
 
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
 
-setTime()
+setTime();
 
-setInterval(setTime, 1000)
+setInterval(setTime, 1000);
